b0111d5f6cebd17aec39e40b73d763d3a2523df3
zagamog <sparandekar@yahoo.com.ar>
2020-01-08 22:57:50 +0530

    Suhas 3rd Chennai commit request EM for Weber by Year

--- wp1/extension2a.R
+++ wp1/extension2a.R
@@ -214,6 +214,209 @@ edu_exp_mean <- cbind.data.frame(Statistic, edu_exp_mean)
 # Latex 
 xtable::xtable(edu_exp_mean)
 
+#######################################
+########### A1F. General model - Female
+stargazer(lm_dep_f[1],
+          lm_dep_f[2],
+          lm_dep_f[3],
+          lm_dep_f[4],
+          lm_dep_f[5],
+          lm_dep_f[6],
+          type = "latex",
+          dep.var.caption = "",
+          dep.var.labels.include = F,
+          df = F,
+          header = F,
+          intercept.bottom = F,
+          se = list(as.vector(summary(lm_dep_f[[1]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_f[[2]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_f[[3]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_f[[4]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_f[[5]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_f[[6]],robust = T)$coefficients[,"Std. Error"])),
+          column.labels = c("1994", "1998", "2003", "2006", "2012", "2018"),
+          covariate.labels = c("Constant",
+                               "Educ, years",
+                               "Exper",
+                               "Exper squared",
+                               "Educ X Exper"))
+
+
+#############
+##############
+########### A2F. General model  - By gender - female 
+# Averages and 3 rows of Vignoli
+library(tables)
+
+# Coefficients for exper
+exp_2_coef <- c(as.vector(lm_dep_f[[1]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_f[[2]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_f[[3]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_f[[4]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_f[[5]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_f[[6]]$coefficients['I(exper^2)']))
+
+# Coefficients for edu_yrs
+edu_yrs_coef <- c(as.vector(lm_dep_f[[1]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_f[[2]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_f[[3]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_f[[4]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_f[[5]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_f[[6]]$coefficients['edu_yrs:exper']))
+
+# Examine the significance
+confint(lm_dep_f[[1]], level = 0.99)
+confint(lm_dep_f[[2]])
+confint(lm_dep_f[[3]])
+confint(lm_dep_f[[4]], level = 0.99)
+confint(lm_dep_f[[5]])
+confint(lm_dep_f[[6]], level = 0.9)
+
+# creating df for further averaging edu_yrs and exper by years
+df_mincer2f <- df_mincer2 %>% filter(female==1)
+edu_exper_f <- df_mincer2f[df_mincer2f$YEAR %in% 
+                          seq_year, c("YEAR", "edu_yrs", "exper")]
+
+# Combining averages and coefficients to a data.frame
+edu_exp_mean <- rbind(
+  dcast(melt(edu_exper_f, id.vars = "YEAR"),
+        variable ~ YEAR,
+        fun.aggregate = mean)[,-1],
+  exp_2_coef,
+  edu_yrs_coef)
+
+# Computing depreciation rates by formulas: 2*pi_2*T and pi_1*S
+# exper
+edu_exp_mean[3,] <- abs(edu_exp_mean[3,]*2*edu_exp_mean[1,]*100)
+
+# edu_yrs
+edu_exp_mean[4,] <- abs(edu_exp_mean[4,]*edu_exp_mean[2,]*100)
+
+# From 2003 to 2018 DR is non-significant => 0
+edu_exp_mean[4, 4] <- 0
+edu_exp_mean[4, 5] <- 0
+edu_exp_mean[4, 6] <- 0
+
+# Human Capital
+edu_exp_mean[5,] <- edu_exp_mean[3,] + edu_exp_mean[4,]
+
+# Rounding
+edu_exp_mean <- round(edu_exp_mean, 2)
+
+# Naming
+Statistic <- c("Experience, mean",
+               "Education, mean",
+               "DR Experience, %",
+               "DR Education, %",
+               "DR Human Capital, %")
+# Adding rownames
+edu_exp_mean <- cbind.data.frame(Statistic, edu_exp_mean)
+# Latex 
+xtable::xtable(edu_exp_mean)
+
+#######################################
+########### A1M. General model - Male
+stargazer(lm_dep_m[1],
+          lm_dep_m[2],
+          lm_dep_m[3],
+          lm_dep_m[4],
+          lm_dep_m[5],
+          lm_dep_m[6],
+          type = "latex",
+          dep.var.caption = "",
+          dep.var.labels.include = F,
+          df = F,
+          header = F,
+          intercept.bottom = F,
+          se = list(as.vector(summary(lm_dep_m[[1]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_m[[2]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_m[[3]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_m[[4]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_m[[5]],robust = T)$coefficients[,"Std. Error"]),
+                    as.vector(summary(lm_dep_m[[6]],robust = T)$coefficients[,"Std. Error"])),
+          column.labels = c("1994", "1998", "2003", "2006", "2012", "2018"),
+          covariate.labels = c("Constant",
+                               "Educ, years",
+                               "Exper",
+                               "Exper squared",
+                               "Educ X Exper"))
+
+
+#############
+##############
+########### A2M. General model  - By gender - Male 
+# Averages and 3 rows of Vignoli
+library(tables)
+
+# Coefficients for exper
+exp_2_coef <- c(as.vector(lm_dep_m[[1]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_m[[2]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_m[[3]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_m[[4]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_m[[5]]$coefficients['I(exper^2)']),
+                as.vector(lm_dep_m[[6]]$coefficients['I(exper^2)']))
+
+# Coefficients for edu_yrs
+edu_yrs_coef <- c(as.vector(lm_dep_m[[1]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_m[[2]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_m[[3]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_m[[4]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_m[[5]]$coefficients['edu_yrs:exper']),
+                  as.vector(lm_dep_m[[6]]$coefficients['edu_yrs:exper']))
+
+# Examine the significance
+confint(lm_dep_m[[1]], level = 0.99)
+confint(lm_dep_m[[2]])
+confint(lm_dep_m[[3]])
+confint(lm_dep_m[[4]], level = 0.99)
+confint(lm_dep_m[[5]])
+confint(lm_dep_m[[6]], level = 0.9)
+
+# creating df for further averaging edu_yrs and exper by years
+df_mincer2m <- df_mincer2 %>% filter(female==0)
+edu_exper_m <- df_mincer2m[df_mincer2f$YEAR %in% 
+                             seq_year, c("YEAR", "edu_yrs", "exper")]
+
+# Combining averages and coefficients to a data.frame
+edu_exp_mean <- rbind(
+  dcast(melt(edu_exper_m, id.vars = "YEAR"),
+        variable ~ YEAR,
+        fun.aggregate = mean)[,-1],
+  exp_2_coef,
+  edu_yrs_coef)
+
+# Computing depreciation rates by formulas: 2*pi_2*T and pi_1*S
+# exper
+edu_exp_mean[3,] <- abs(edu_exp_mean[3,]*2*edu_exp_mean[1,]*100)
+
+# edu_yrs
+edu_exp_mean[4,] <- abs(edu_exp_mean[4,]*edu_exp_mean[2,]*100)
+
+# From 2003 to 2018 DR is non-significant => 0
+edu_exp_mean[4, 4] <- 0
+edu_exp_mean[4, 5] <- 0
+edu_exp_mean[4, 6] <- 0
+
+# Human Capital
+edu_exp_mean[5,] <- edu_exp_mean[3,] + edu_exp_mean[4,]
+
+# Rounding
+edu_exp_mean <- round(edu_exp_mean, 2)
+
+# Naming
+Statistic <- c("Experience, mean",
+               "Education, mean",
+               "DR Experience, %",
+               "DR Education, %",
+               "DR Human Capital, %")
+# Adding rownames
+edu_exp_mean <- cbind.data.frame(Statistic, edu_exp_mean)
+# Latex 
+xtable::xtable(edu_exp_mean)
+
+
+
+
 ###########
 ########### B.I By industry and B.II By 2-digit occupations
 ###########
@@ -369,5 +572,8 @@ edu_exp_mean <- rbind.data.frame(col, edu_exp_mean)
 
 # Latex 
 xtable::xtable(edu_exp_mean)
+
+
+#### By gender - 
 
 
